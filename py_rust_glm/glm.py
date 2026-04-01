85821b78585e7cec109dfc6c8ec701d3b90104c3
Matthew Drury <matthew.drury.83@gmail.com>
2026-04-01 16:01:15 -0700

    Poisson GLM works.

--- py_rust_glm/glm.py
+++ py_rust_glm/glm.py
@@ -4,11 +4,12 @@ from typing import Self, TypeAlias
 import numpy as np
 from numpy.typing import NDArray
 
-from py_rust_glm._core import solve, solvelr, score, scorelr
+from py_rust_glm._core import solve, solvelr, score, scorelr, solvepr, scorepr
 
 
 DesignMatrix: TypeAlias = NDArray[np.float64]
 Target: TypeAlias = NDArray[np.float64]
+Exposure: TypeAlias = NDArray[np.float64]
 Coefficients: TypeAlias = NDArray[np.float64]
 Predictions: TypeAlias = NDArray[np.float64]
 
@@ -18,7 +19,7 @@ class LinearRegression:
     def __init__(self):
         self.coef_: Coefficients | None = None
 
-    def fit(self, X: DesignMatrix, y: DesignMatrix) -> Self:
+    def fit(self, X: DesignMatrix, y: Target) -> Self:
         self.coef_ = solve(X, y)
         return self
 
@@ -33,7 +34,7 @@ class LogisticRegression:
     def __init__(self):
         self.coef_: Coefficients | None = None
 
-    def fit(self, X: DesignMatrix, y: DesignMatrix) -> Self:
+    def fit(self, X: DesignMatrix, y: Target) -> Self:
         self.coef_ = solvelr(X, y)
         return self
 
@@ -41,3 +42,22 @@ class LogisticRegression:
         if self.coef_ is None:
             raise RuntimeError("Call fit() before predict()")
         return scorelr(X, self.coef_)
+
+
+class PoissonRegression:
+
+    def __init__(self):
+        self.coef_: Coefficients | None = None
+
+    def fit(self, X: DesignMatrix, y: Target, t: Exposure | None = None) -> Self:
+        if t is None:
+            t = np.ones(shape=X.shape[0], dtype=np.float64)
+        self.coef_ = solvepr(X, y, t)
+        return self
+
+    def predict(self, X: DesignMatrix, t: Exposure | None = None) -> Predictions:
+        if self.coef_ is None:
+            raise RuntimeError("Call fit() before predict()")
+        if t is None:
+            t = np.ones(shape=X.shape[0], dtype=np.float64)
+        return scorepr(X, self.coef_, t)
--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ use pyo3::prelude::*;
 const MAX_ITER: usize = 10;
 const TOL: f64 = 1e-8;
 
+
 #[pyfunction]
 fn solve<'py>(
     py: Python<'py>,
@@ -40,6 +41,7 @@ fn score<'py>(
     let y_hat = X * β;
     Ok(PyArray1::from_slice(py, y_hat.as_slice()))
 }
+
 
 #[pyfunction]
 fn solvelr<'py>(
@@ -98,11 +100,75 @@ fn scorelr<'py>(
     Ok(PyArray1::from_slice(py, p.as_slice()))
 }
 
+#[pyfunction]
+fn solvepr<'py>(
+    py: Python<'py>,
+    X: PyReadonlyArray2<'py, f64>,
+    y: PyReadonlyArray1<'py, f64>,
+    t: PyReadonlyArray1<'py, f64>,
+) -> PyResult<Bound<'py, PyArray1<f64>>> {
+    let n_samples = X.shape()[0];
+    let n_features = X.shape()[1];
+    let X = DMatrix::from_row_slice(
+        n_samples,
+        n_features,
+        X.as_slice().expect("Array must be contiguous"),
+    );
+    let y = DVector::from_row_slice(y.as_slice().expect("Array must be contiguous"));
+    let t = DVector::from_row_slice(t.as_slice().expect("Array must be contiguous"));
+    let Xt = X.transpose();
+
+    let mut β: DVector<f64> = DVector::zeros(n_features);
+    let mut r: DVector<f64>;
+    let mut ν: DVector<f64>;
+    let mut λ: DVector<f64>;
+
+    for _ in 0..MAX_ITER {
+        ν = &X * &β;
+        λ = ν.map(|νi| νi.exp());
+        r = DVector::from_fn(n_samples, |i, _| {y[i] - λ[i] * t[i]});
+        let Xtr = &Xt * &r;
+        let Xw = DMatrix::from_fn(n_samples, n_features, |i, j| {
+            X[(i, j)] * λ[i] * t[i]
+        });
+        let wXtX = Xw.transpose() * &X;
+        let chol = wXtX.cholesky().ok_or_else(|| {
+            PyValueError::new_err("wXᵀX is not positive definite")
+        })?;
+        β = β + chol.solve(&Xtr);
+    }
+    Ok(PyArray1::from_slice(py, β.as_slice()))
+}
+
+#[pyfunction]
+fn scorepr<'py>(
+    py: Python<'py>,
+    X: PyReadonlyArray2<'py, f64>,
+    t: PyReadonlyArray1<'py, f64>,
+    β: PyReadonlyArray1<'py, f64>,
+) -> PyResult<Bound<'py, PyArray1<f64>>> {
+    let n_samples = X.shape()[0];
+    let n_features = X.shape()[1];
+    let X = DMatrix::from_row_slice(
+        n_samples,
+        n_features,
+        X.as_slice().expect("Array must be contiguous"),
+    );
+    let t = DVector::from_row_slice(t.as_slice().unwrap());
+    let β = DVector::from_row_slice(β.as_slice().unwrap());
+    let ν = X * β;
+    let λ = ν.map(|νi| νi.exp());
+    Ok(PyArray1::from_slice(py, (λ * t).as_slice()))
+}
+
+
 #[pymodule]
 fn _core(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(solve, m)?)?;
     m.add_function(wrap_pyfunction!(score, m)?)?;
     m.add_function(wrap_pyfunction!(solvelr, m)?)?;
     m.add_function(wrap_pyfunction!(scorelr, m)?)?;
+    m.add_function(wrap_pyfunction!(solvepr, m)?)?;
+    m.add_function(wrap_pyfunction!(scorepr, m)?)?;
     Ok(())
 }
